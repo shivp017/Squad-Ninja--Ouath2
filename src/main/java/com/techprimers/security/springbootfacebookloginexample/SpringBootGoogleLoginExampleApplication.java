3a41a7e997ce0877a6ba9e9788d01ce1a7544c0c
shiv.prakash <shiv.prakash@cgi.com>
2019-08-19 22:24:11 +0530

    ouath2

---
+++ src/main/java/com/techprimers/security/springbootfacebookloginexample/SpringBootGoogleLoginExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.security.springbootfacebookloginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleLoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGoogleLoginExampleApplication.class, args);
+	}
+
+}
